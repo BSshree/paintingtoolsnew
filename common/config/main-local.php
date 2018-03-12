fb6b510a8a601786b8e05d545b645efc820a90b6
vasanthark <vasanth@arkinfotec.com>
2018-03-12 11:07:29 +0530

    Intial Commit

---
+++ common/config/main-local.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=paintingtool', //rajencba_paintings
+            'username' => 'root', //rajencba_paint
+            'password' => '', //uC}},n)Ke2wc
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+            'transport' => [
+               'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'sumanasdev@gmail.com',
+                'password' => 'sum@n@stech',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
+        ],
+    ],
+];
